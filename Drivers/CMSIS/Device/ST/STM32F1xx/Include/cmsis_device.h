0d0618069faa87877be052063fb7b405722e51a3
Ryan Pavlik <ryan@sensics.com>
2017-09-04 15:52:12 -0500

    Add cmsis_device.h

---
+++ Drivers/CMSIS/Device/ST/STM32F1xx/Include/cmsis_device.h
@@ -0,0 +1,11 @@
+//
+// Copyright (c) 2015 Liviu Ionescu.
+// This file is part of the xPacks project (https://xpacks.github.io).
+//
+
+#ifndef STM32F1_CMSIS_DEVICE_H_
+#define STM32F1_CMSIS_DEVICE_H_
+
+#include "stm32f1xx.h"
+
+#endif // STM32F1_CMSIS_DEVICE_H_
