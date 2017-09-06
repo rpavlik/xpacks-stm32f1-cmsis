5b4f9dfdcc1a11163cc64c8a0620f50649a2088d
Ryan Pavlik <ryan@sensics.com>
2017-09-06 12:01:07 -0500

    Add generate-xpdsc.sh modified from the f4 version

---
+++ scripts/generate-xpdsc.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+if [ ! -f xpack.json ]
+then
+	echo "Must be started in a package root folder."
+	exit 1
+fi
+
+PDSC_FILE="Keil.STM32F1xx_DFP.pdsc"
+xcdl pdsc-convert --file ${PDSC_FILE} --output xpdsc.json
